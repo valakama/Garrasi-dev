ce9302d984fcd93b3f540e0c27ddacf2d3f1a376
valakama <valentin.duban@wanadoo.fr>
2024-01-17 11:37:21 +0100

    [~] homepage

--- garrasi-react/src/components/Header.js
+++ garrasi-react/src/components/Header.js
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom"
 const Header = () => {
     return (
-        <header>
-            <h1></h1>
-        </header>
+        <div className={`flex flex-row justify-between py-5 px-10 items-center  relative`}>
+        <Link to={"/"} >
+          <div className=''>
+            <h1 className='text-3xl font-extrabold'>Area</h1>
+          </div>
+        </Link>
+        <ul className={`xl:flex  flex flex-col absolute top-0 left-0 w-full h-[100vh] z-2 justify-center lg:h-auto bg-black bg-opacity-80 lg:bg-transparent text-center lg:w-auto xl:relative items-center xl:flex-row `}>
+        <Link to={"/explore"} >
+          <li className=' lg:mx-8 text-xl'>Mes Automatisations</li>
+        </Link>
+        <Link to={"/create"}>
+          {/* <li ><ButtonAction color='#27917E' text='Automatiser'/></li> */}
+        </Link>
+        </ul>
+      </div>
     )
 }
+
+export default Header
